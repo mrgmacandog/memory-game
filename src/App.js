064b44b1355074f44fdb110ecbd45309accda8f5
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-15 18:02:53 -0700

    Update App.js to become a stateful component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import ClickImage from "./components/ClickImage";
 import characters from "./characters.json";
 
-function App() {
-	return (
-		<div className="container App">
-			<div className="row">
-				{characters.map(character =>
-					<ClickImage name={character.name} image={character.image}/>
-				)}
+class App extends Component {
+	render() {
+		return (
+			<div className="container App">
+				<div className="row">
+					{characters.map(character =>
+						<ClickImage name={character.name} image={character.image}/>
+					)}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
