19ad8ede44416a92f9cd9b523aabbca0b661d765
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-16 12:19:09 -0700

    Add updateScore method to ClickImage onClick

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import ClickImage from "./components/ClickImage";
 import characters from "./characters.json";
 
 class App extends Component {
+
+	// App state
+	state = {
+		currScore: 0,
+		highScore: 0
+	}
+
+	/**
+	 * Updates the currScore depending on the id
+	 * @param {int} id character id
+	 */
+	updateScore(id) {
+		alert(`You chose ${id}`);
+	};
+
 	render() {
 		return (
 			<div className="container App">
 				<div className="row">
 					{characters.map(character =>
-						<ClickImage name={character.name} image={character.image}/>
+						<ClickImage
+							key={character.id}
+							id={character.id}
+							name={character.name}
+							image={character.image}
+							updateScore={this.updateScore}
+						/>
 					)}
 				</div>
 			</div>
--- src/components/ClickImage/index.js
+++ src/components/ClickImage/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function ClickImage(props) {
     return (
-        <div className="col-md-3">
+        <div className="col-md-3" onClick={() => props.updateScore(props.id)}>
             <div className="card">
                 {/* <div className="img-container"> */}
                     <img alt={props.name} src={props.image} />
