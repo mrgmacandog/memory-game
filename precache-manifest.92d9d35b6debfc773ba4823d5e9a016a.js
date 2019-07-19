451f0bd4df4c26cf930a3e06a3f828e2678f579a
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-19 13:26:56 -0700

    Updates

---
+++ precache-manifest.92d9d35b6debfc773ba4823d5e9a016a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cf9d580824ea6005682bb147593e0b",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "0edb5d3013f4ff57db07",
+    "url": "/memory-game/static/css/main.c13c0ac3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0edb5d3013f4ff57db07",
+    "url": "/memory-game/static/js/main.017a9975.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
